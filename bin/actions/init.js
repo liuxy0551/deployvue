067f6374e39851b9fbda143b5b1c8876b4d4deea
liuyi <liuxy0551@qq.com>
2020-08-14 15:21:29 +0800

    Update init config.js with fs.copyFile

--- bin/actions/init.js
+++ bin/actions/init.js
@@ -6,14 +6,18 @@ module.exports = async function () {
   let exist = fs.existsSync('deploy.config.js')
   if (!exist) {
     // 初始化项目的部署配置文件
-    fs.readFile(`${ __dirname.split('bin/')[0] }/init/deploy.config.js`, function (error, data) {
-      fs.writeFile(`${ process.cwd() }/deploy.config.js`, data, function (err) {
-        if (err) {
-          throw err
-        }
-        console.log(chalk.bgGreen(`DONE`), chalk.green(`init success`))
-      })
-    })
+    fs.copyFileSync(`${ __dirname.split('bin/')[0] }/init/deploy.config.js`, `${ process.cwd() }/deploy.config.js`)
+    console.log(chalk.bgGreen(`DONE`), chalk.green(`init success`))
+
+    // 也可以通过 readFile 和 writeFile 实现
+    // fs.readFile(`${ __dirname.split('bin/')[0] }/init/deploy.config.js`, function (error, data) {
+    //   fs.writeFile(`${ process.cwd() }/deploy.config.js`, data, function (err) {
+    //     if (err) {
+    //       throw err
+    //     }
+    //     console.log(chalk.bgGreen(`DONE`), chalk.green(`init success`))
+    //   })
+    // })
   } else {
     console.log(chalk.yellow(`Fail: deploy.config.js already exist`))
   }
